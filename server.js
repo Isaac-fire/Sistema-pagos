e3ed3188270f537131c9214bccb5eb809d8bb717
Isaac-fire <aicatalyst.business@gmail.com>
2026-07-21 04:36:18 +0000

    Subir archivos limpios a la raiz

---
+++ server.js
@@ -0,0 +1,1 @@
+const express = require("express"); const app = express(); const PORT = process.env.PORT || 3000; app.get("/", (req, res) => res.send("<h1>Sistema Activo</h1>")); app.listen(PORT, () => console.log("Servidor listo"));
